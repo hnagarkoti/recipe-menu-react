b00ea4fd65859f923ebbcc16ac1cb2027689b4a1
hnagarkoti <hemantnagarkoti5@gmail.com>
2017-07-23 15:39:55 +0530

    footer added

--- src/PageLayout/Footer.js
+++ src/PageLayout/Footer.js
@@ -1,13 +1,43 @@
 import React, {Component} from 'react';
-
+import {
+  Grid,
+  Row,
+  Col,
+  Image,
+  ListGroup,
+  ListGroupItem
+} from 'react-bootstrap/dist/react-bootstrap.js';
 class Footer extends Component {
   render(){
+    const footerSection ={
+      padding: 100
+    }
     return(
-      <div>
-        <footer>Copyright &copy; Hemant Nagarkoti</footer>
+      <div style={footerSection}>
+        <Grid>
+         <Row className="show-grid">
+           <Col xs={6} md={4}><code>
+             <Image src="http://localhost:8080/Blog_Map.jpg" responsive />
+           </code></Col>
+           <Col xs={6} md={4}><code>
+             <h3>Quick Links</h3>
+               <ListGroup>
+                <ListGroupItem href="#link1">Existing Members? Login </ListGroupItem>
+                <ListGroupItem href="#link2">Blogs</ListGroupItem>
+                <ListGroupItem href="#link2">Webinars</ListGroupItem>
+              </ListGroup>
+           </code></Col>
+           <Col xsHidden md={4}><code>
+             <h3>Download Free E-Book</h3>
+           </code></Col>
+         </Row>
+         <footer>Copyright &copy; Hemant Nagarkoti</footer>
+       </Grid>
       </div>
     )
   }
 }
 
 export default Footer;
+
+// <footer>Copyright &copy; Hemant Nagarkoti</footer>
