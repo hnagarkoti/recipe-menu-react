60d1983dc4d66a15fa120f000d8fe580d28b578b
hemant <hemant.nagarkoti@armentum.co>
2018-01-05 01:10:39 +0530

    deleting recipe fun. added

--- src/Recipe/RecipeRow.js
+++ src/Recipe/RecipeRow.js
@@ -10,8 +10,29 @@ import {
  } from 'react-bootstrap/dist/react-bootstrap.js';
 
  import PopularPost from '../Posts/PopularPost';
-
+import config from '../config.js';
 class RecipeRow extends Component {
+  constructor(props){
+    super(props);
+    // this.handleDelete = this.handleDelete.bind(this);
+  }
+  handleDelete(id){
+    console.log('ok delete id ', id);
+
+      fetch( config.host + config.middleware + '/recipe/'+id, {
+          method: 'DELETE',
+          headers: {
+            Accept: 'application/json',
+          },
+        }).then((res) => {
+          console.log('delted successfully!');
+          alert('successfully deleted! Please refresh ur page or we can update state also in this case')
+        })
+        .catch((err) => {
+          console.log('error while deleting!!');
+          alert("Some problem while deleting recipe!")
+        })
+  }
   render(){
     var that = this;
     return(
@@ -31,6 +52,9 @@ class RecipeRow extends Component {
                         <article>
                           <p>{ item.description ? item.description.substring(0,200) : 'Description Not Available' }</p>
                           <Link to={'/recipe/' + item._id}> Read More </Link>
+                        </article>
+                        &nbsp;&nbsp;<article>
+                          <p onClick={that.handleDelete.bind(this, item._id)}> Delete Recipe </p>
                         </article>
                       </div>
                   </div>
